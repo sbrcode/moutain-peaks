ffefa66cf812ca5b166d56bedc7da33ac387886d
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-04-06 15:27:30 +0200

    end-details-page

--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class PeakLocation(models.Model):
     name = models.CharField(max_length=200)
-    lat = models.IntegerField(default=0)
-    lon = models.IntegerField(default=0)
+    lat = models.FloatField(default=0)
+    lon = models.FloatField(default=0)
     altitude = models.IntegerField(default=0)
     def __str__(self):
         return self.name
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import PeakLocation
 from django.template import loader
 # Create your views here.
